815f76cbd5933b56220615c780adb230e00698d0
Pranesh-vimal <Pranesh.vimal5@gmail.com>
2020-03-15 20:01:51 +0530

    button transition removed

--- src/components/Content.js
+++ src/components/Content.js
@@ -44,7 +44,6 @@ export default class Content extends Component {
 
               <div className="flex">
                 <a
-                  data-aos="zoom-in"
                   href="/"
                   className="text-center flex-1 text-white bg-red-500 p-2 rounded-md w-full hover:bg-red-600"
                 >
@@ -63,7 +62,6 @@ export default class Content extends Component {
               </div>
               <div className="flex">
                 <a
-                  data-aos="zoom-in"
                   href="/"
                   className="text-center flex-1 text-white bg-red-500 mt-2 p-2 rounded-md w-full hover:bg-red-600"
                 >
